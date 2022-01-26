4c1b42254b574064b494a86eb3f0109aa34dabc2
70/V9 <tong@disktree.net>
2022-01-26 11:57:23 +0100

    Add polybar mcabber script

---
+++ .config/polybar/scripts/mcabber.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+FILE=$HOME/.mcabber/mcabber.state
+if [ -f $FILE ];then
+    NUM_UNREAD="$(grep -e "$" -c $FILE)"
+    if [[ $NUM_UNREAD == 1 ]];then
+        JID=$(cat $FILE)
+        echo " $JID"
+    else
+        echo "ﮠ $NUM_UNREAD"
+    fi
+fi
