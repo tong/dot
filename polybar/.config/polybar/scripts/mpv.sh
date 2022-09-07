a83a2921c1dc7ceff038c85d42cd846835f88e31
tong <tong@disktree.net>
2022-09-08 01:47:44 +0200

    Polybar mpv script fix

--- polybar/.config/polybar/scripts/mpv.sh
+++ polybar/.config/polybar/scripts/mpv.sh
@@ -4,10 +4,12 @@ _print_time() {
 }
 if pidof mpv > /dev/null; then
     SOCK='/tmp/mpvsocket'
-    metadata=$(echo '{ "command": ["get_property", "media-title"] }' | socat - $SOCK | jq -r '.data')
+    metadata=$(echo '{ "command": ["get_property", "media-title"] }' | socat - $SOCK | jq -r .data)
     position=$(echo '{ "command": ["get_property_string", "time-pos"] }' | socat - $SOCK  | jq -r .data | cut -d'.' -f 1)
     remaining=$(echo '{ "command": ["get_property_string", "time-remaining"] }' | socat - $SOCK | jq -r .data | cut -d'.' -f 1)
     printf '%s' "$metadata"
     _print_time '|' $((position/3600)) $((position%3600/60)) $((position%60))
     _print_time '-' $((remaining/3600)) $((remaining%3600/60)) $((remaining%60))
+else
+    exit 0
 fi
