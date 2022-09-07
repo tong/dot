08c67f9d9c58692072c7f53a71f21f55ae7000b7
tong <tong@disktree.net>
2022-09-07 13:42:49 +0200

    Cleanup polybar mpv script

--- polybar/.config/polybar/scripts/mpv.sh
+++ polybar/.config/polybar/scripts/mpv.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+_print_time() {
+    printf ' %s %d:%02d:%02d' "$1" "$2" "$3" "$4"
+}
 if pidof mpv > /dev/null; then
-    position=$(echo '{ "command": ["get_property_string", "time-pos"] }' | socat - /tmp/mpvsocket | jq .data | tr '"' ' ' | cut -d'.' -f 1)
-    remaining=$(echo '{ "command": ["get_property_string", "time-remaining"] }' | socat - /tmp/mpvsocket | jq .data | tr '"' ' ' | cut -d'.' -f 1)
-    metadata=$(echo '{ "command": ["get_property", "media-title"] }' | socat - /tmp/mpvsocket | cut -b 10-30)
+    SOCK='/tmp/mpvsocket'
+    metadata=$(echo '{ "command": ["get_property", "media-title"] }' | socat - $SOCK | jq -r '.data')
+    position=$(echo '{ "command": ["get_property_string", "time-pos"] }' | socat - $SOCK  | jq -r .data | cut -d'.' -f 1)
+    remaining=$(echo '{ "command": ["get_property_string", "time-remaining"] }' | socat - $SOCK | jq -r .data | cut -d'.' -f 1)
     printf '%s' "$metadata"
-	printf ' | %d:%02d:%02d' $((position/3600)) $((position%3600/60)) $((position%60))
-	printf ' - %d:%02d:%02d\n' $((remaining/3600)) $((remaining%3600/60)) $((remaining%60))
-else
-    exit
+    _print_time '|' $((position/3600)) $((position%3600/60)) $((position%60))
+    _print_time '-' $((remaining/3600)) $((remaining%3600/60)) $((remaining%60))
 fi
