5e843252aa8810629b63640603a7255139bf4944
tong <tong@disktree.net>
2022-09-09 11:58:08 +0200

    Add zsh stats function

--- zsh/.config/zsh/functions.zsh
+++ zsh/.config/zsh/functions.zsh
@@ -1,3 +1,11 @@
+
+function zshstats() {
+    local num=${1:-50}
+    fc -l 1 \
+        | awk '{ CMD[$2]++; count++; } END { for (a in CMD) print CMD[a] " " CMD[a]*100/count "% " a }' \
+        | grep -v "./" | sort -nr | head -n $num | column -c3 -s " " -t | nl
+}
+
 cd() {
     if (( $+2 )); then
         builtin cd "$@"
