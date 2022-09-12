05280de6675b86f36f9036e8ac0698859ecc4d9c
tong <tong@disktree.net>
2022-09-13 00:08:17 +0200

    Update tmux polybar script

--- polybar/.config/polybar/scripts/tmux.sh
+++ polybar/.config/polybar/scripts/tmux.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-if sessionlist=$(tmux ls 2>/dev/null); then
-    echo "$sessionlist" | while read -r line; do
+if sessions=$(tmux ls 2>/dev/null); then
+    echo "$sessions" | while read -r line; do
         session=$(echo "$line" | cut -d ':' -f 1)
         if echo "$line" | grep -q "(attached)"; then
             color="%{B#00000000}%{F#e0e0e0}"
+            printf "<%s%s%s>" "$color" "$session" "%{B- F-}"
         else
             color="%{B#00000000}%{F#9E9E9E}"
+            printf " %s%s%s" "$color" "$session" "%{B- F-}"
         fi
-        printf "%s%s %s" "$color" "$session" "%{B- F-}"
     done
+else
+    echo ""
 fi
