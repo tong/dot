2b02303fb6b815dcbcf6caca92cac9b292f7bbf0
tong <tong@disktree.net>
2022-09-21 22:15:09 +0200

    Update picom grayscale shader

--- picom/.config/picom/grayscale.glsl
+++ picom/.config/picom/grayscale.glsl
@@ -3,7 +3,11 @@ uniform bool invert_color;
 uniform sampler2D tex;
 
 void main() {
-	vec3 luma = vec3(.299, 0.587, 0.114);
 	vec4 c = texture2D(tex, gl_TexCoord[0].xy);
-	gl_FragColor = vec4(vec3(dot(c.rgb, luma)), c.a);;
+	float g = 0.2126 * c.r + 0.7152 * c.g + 0.0722 * c.b;   // CIELAB luma, based on human tristimulus.
+	c = vec4(vec3(g), c.a);
+ 	if (invert_color)
+		c = vec4(vec3(c.a, c.a, c.a) - vec3(c), c.a);
+	c *= opacity;
+	gl_FragColor = c;
 }
