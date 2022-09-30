eca9d6afc7a4de69cafd63909e50e734a2a45929
tong <tong@disktree.net>
2022-09-30 13:00:42 +0200

    Update zsh aliases

--- zsh/.config/zsh/aliases_user.zsh
+++ zsh/.config/zsh/aliases_user.zsh
@@ -1,10 +1,9 @@
+#alias mpv='i3-swallow mpv'
+alias neovide='i3-swallow neovide'
+alias sxiv='i3-swallow sxiv'
 
 alias lf='$HOME/.config/lf/lfrun'
 alias lynx='lynx -vikeys -accept_all_cookies'
-alias lynx='lynx -cfg $HOME/.config/lynx/lynx.cfg'
-alias mpv='i3-swallow mpv'
-alias neovide='i3-swallow neovide'
-alias sxiv='i3-swallow sxiv'
 
 alias alacritty-theme='python $HOME/.config/alacritty/alacritty-theme.py $@'
 alias calc='bc -q $@'
@@ -18,39 +17,43 @@ alias notify='notify-send'
 alias pdf='zathura'
 alias screenshot='scrot -u $SCREENSHOTS/screenshot-%Y_%m_%d_%H_%M.png'
 alias screen-off='xset dpms force off'
+alias ultraviolet='$EDITOR -c "VimwikiIndex"'
 alias weather='curl -L wttr.in'
 alias web='lynx'
 alias ydl-mp3='yt-dlp --ignore-errors --output "%(title)s.%(ext)s" --extract-audio --audio-format mp3 "$@"'
 
-alias '?'='duckduckgo'
 alias bc="bc -ql"
-alias ddg="duckduckgo"
+#alias ddg="duckduckgo"
+alias ddg="ddgr"
 alias ext='extract'
 alias fm='lf'
+alias ta='task'
+alias tm='timew'
 alias tt='taskwarrior-tui'
-alias tw='task'
 alias tx='tmux'
-alias ultraviolet='$EDITOR -c "VimwikiIndex"'
 alias v='$EDITOR' 
 alias vv='$EDITOR .'
 alias x='extract'
-
+alias '?'='ddg'
 ##alias doc='$EDITOR -c "VimwikiGoto Doc'
 
 alias ydl='yt-dlp'
 alias ydl-mp3='yt-dlp --ignore-errors --output "%(title)s.%(ext)s" --extract-audio --audio-format mp3 "$@"'
 
+alias -s hxml=haxe
+alias -s js=node
+
 alias closure-stylesheets='java -jar $HOME/sdk/closure-stylesheets.jar'
 alias lessc-live='neko $DEV/tool/lessc-live/lessc-live.n $@'
 alias luamake='$HOME/src/lua-language-server/3rd/luamake/luamake $@'
 
+alias armory='blender --app-template armory'
 alias krom='$ARMSDK/Krom/Krom'
 alias khamake='node $ARMSDK/Kha/Tools/khamake/khamake.js'
 alias kmake='$ARMSDK/Kha/Kinc/Tools/linux_x64/kmake'
 #alias armory='blender -b --python $ARMSDK/armory/armory-cli.py $1'
 #alias armory='blender -b $@ --python $ARMSDK/armory/armory-cli.py'
 #alias armory='python $ARMSDK/armory/armory-cli.py $@'
-#alias armory='blender --app-template armory $@'alias v="$EDITOR $@"
 
 alias arm='cd $HOME/dev/armory3d/'
 alias dev='cd $HOME/dev'
