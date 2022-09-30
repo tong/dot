de200d1d72ef70974558b68d256aee176a97dee2
tong <tong@disktree.net>
2022-10-01 01:01:30 +0200

    Update taskwarrior polybar integration

--- polybar/.config/polybar/scripts/taskwarrior.sh
+++ polybar/.config/polybar/scripts/taskwarrior.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+current_context=$(task _get rc.context)
 n_active=$(task +ACTIVE count)
 n_pending=$(task status:pending count)
 n_next=$(task status:pending +next count)
@@ -12,33 +13,33 @@ n_overdue=$(task +READY +OVERDUE count)
     #n_overdue=" OVERDUE:$n_overdue"
 #fi
 
-#active_project=""
-declare active_project
-decalare active_description
+str="%{F#9E9E9E}"
+
 if [ "$n_active" -ne 0 ]; then
+
     active_project=$(task active rc.color:off rc.verbose: rc.report.active.columns:project rc.report.active.labels:1)
     active_description=$(task active rc.color:off rc.verbose: rc.report.active.columns:description rc.report.active.labels:1)
-    #active_tags=$(task active rc.verbose: rc.report.active.columns:tags rc.report.active.labels:1)
+    active_tags=$(task active rc.verbose: rc.report.active.columns:tags rc.report.active.labels:1)
+
+    in=$(timew export | jq -r '.[-1].start')
+    time_start=$(date -d "${in:0:4}-${in:4:2}-${in:6:2} ${in:9:2}:${in:11:2}:${in:13:2}" +%s)
+    time_now=$(date +%s)
+    secs=$((time_now - (time_start+7200))) # TODO HACK for wrong timew config (+2h)
+    time_elapsed=$(printf '%02d:%02d:%02d' $((secs/3600)) $((secs%3600/60)) $((secs%60)))
 fi
 
-str="~$n_active!$n_overdue+$n_next/$n_pending"
-
-if [ "$active_project" ]; then
-    str="$str $active_project"
+[ "$current_context" ] && str="$str $current_context:"
+str+="$n_pending~$n_active+$n_next!$n_overdue"
+[ "$active_description" ] && str+=" %{F#f0f0f0} $active_description "
+[ "$active_project" ] && str+=" %{F#9E9E9E}$active_project"
+if [[ -n $active_tags ]]; then
+    str+="%{F#9E9E9E}"
+    IFS=' ' read -ra tags <<< "$active_tags"
+    for i in "${tags[@]}"
+    do
+        str+=" +$i"
+    done
 fi
-str="$str  $active_description"
-
-#if [ $n_overdue -eq 0 ]; then
-    #str="$str"
-#else
-    #overdue_tasks_text=" "
-    #for i in {0..$n_overdue}; do
-        #task_project=$(task +OVERDUE export | jq -r '.[0] .project')
-        #task_descriptipon=$(task +OVERDUE export | jq -r '.[0] .description')
-        #overdue_tasks_text=" $overdue_tasks_text $task_project:$task_descriptipon"
-    #done
-    #str="$str $overdue_tasks_text  "
-#fi
-
+[ "$time_elapsed" ] && str+=" $time_elapsed"
 echo "$str"
 
