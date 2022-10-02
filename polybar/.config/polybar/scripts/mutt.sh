f4b916ce9cfdd9db4c967761f4f23ce0587c2758
tong <tong@disktree.net>
2022-10-02 21:38:38 +0200

    Update polybar modules

--- polybar/.config/polybar/scripts/mutt.sh
+++ polybar/.config/polybar/scripts/mutt.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #N=$(find $HOME/.local/share/mail/$EMAIL/INBOX/new -type f -newer "${XDG_CONFIG_HOME:-$HOME/.config}/mutt/.mailsynclastrun" 2> /dev/null)
-N=$(find "$HOME/.local/share/mail/$EMAIL/INBOX/new" -type f | wc -l)
-if [ "$N" = "0" ]; then
+UNREAD=$(find "$HOME/.local/share/mail/$EMAIL/INBOX/new" -type f | wc -l)
+if [ "$UNREAD" = "0" ]; then
     echo ""
 else
-    echo ""
+    echo " $UNREAD"
 fi
