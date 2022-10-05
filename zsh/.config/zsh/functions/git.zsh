69753cd36ec8ac5efc3f4b06c363e239928e80bf
tong <tong@disktree.net>
2022-10-05 17:17:07 +0200

    Add gitignore zsh completion

---
+++ zsh/.config/zsh/functions/git.zsh
@@ -0,0 +1,11 @@
+function gitignore() {
+    curl -fLw '\n' https://www.gitignore.io/api/"${(j:,:)@}"
+}
+_gitignoreio_get_command_list() {
+    curl -sfL https://www.gitignore.io/api/list | tr "," "\n"
+}
+_gitignoreio () {
+    compset -P '*,'
+    compadd -S '' `_gitignoreio_get_command_list`
+}
+compdef _gitignoreio gitignore
