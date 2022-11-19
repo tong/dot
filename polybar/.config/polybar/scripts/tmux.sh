4c4cdaf10a733dce94a36f22010c6c9f8c868856
tong <tong@disktree.net>
2022-11-19 20:35:06 +0100

    Cleanup tmux polubar module

--- polybar/.config/polybar/scripts/tmux.sh
+++ polybar/.config/polybar/scripts/tmux.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 if sessions=$(tmux ls 2>/dev/null); then
+    printf " "
     echo "$sessions" | while read -r line; do
         session=$(echo "$line" | cut -d ':' -f 1)
         if echo "$line" | grep -q "(attached)"; then
-            color="%{B#00000000}%{F#e0e0e0}"
-            printf "<%s%s%s>" "$color" "$session" "%{B- F-}"
+            color="%{B#00}%{F#e0e0e0}"
         else
             color="%{B#00000000}%{F#9E9E9E}"
-            printf " %s%s%s" "$color" "$session" "%{B- F-}"
         fi
+        printf "%s%s%s " "$color" "$session" "%{B- F-}"
     done
 else
     echo ""
