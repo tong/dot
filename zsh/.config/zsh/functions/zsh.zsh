a6ba9968de43e8b6fd98b6edff8bb6278dd11374
tong <tong@disktree.net>
2022-11-21 09:26:10 +0100

    Fix exit zsh on non empty lines shortcut

--- zsh/.config/zsh/functions/zsh.zsh
+++ zsh/.config/zsh/functions/zsh.zsh
@@ -1,11 +1,17 @@
+
+# By default, Ctrl+d will not close your shell if the command line is filled, this fixes it
+exit_zsh() { exit }
+zle -N exit_zsh
+bindkey '^D' exit_zsh
+
+zshreload () {
+    exec "${SHELL}" "$@"
+}
+
 zshstats() {
     local num=${1:-50}
     fc -l 1 \
         | awk '{ CMD[$2]++; count++; } END { for (a in CMD) print CMD[a] " " CMD[a]*100/count "% " a }' \
         | grep -v "./" | sort -nr | head -n $num | column -c3 -s " " -t | nl
-}
-
-zshreload () {
-    exec "${SHELL}" "$@"
 }
 
