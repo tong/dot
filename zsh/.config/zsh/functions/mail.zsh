482d7198e9a96063f838b03a23525d16321fc0e5
tong <tong@disktree.net>
2022-12-26 22:20:19 +0100

    Fix zsh mail functions

--- zsh/.config/zsh/functions/mail.zsh
+++ zsh/.config/zsh/functions/mail.zsh
@@ -1,10 +1,10 @@
 mailbox_count() {
     find "$HOME/.local/share/mail/$EMAIL/$1/cur" -type f | wc -l
 }
-alias mail_count_archive='count_mailbox Archive'
-alias mail_count_drafts='count_mailbox Drafts'
-alias mail_count_inbox='count_mailbox INBOX'
-alias mail_count_junk='count_mailbox Junk'
-alias mail_count_sent='count_mailbox Sent'
-alias mail_count_trash='count_mailbox Trash'
+alias mail_count_archive='mailbox_count Archive'
+alias mail_count_drafts='mailbox_count Drafts'
+alias mail_count_inbox='mailbox_count INBOX'
+alias mail_count_junk='mailbox_count Junk'
+alias mail_count_sent='mailbox_count Sent'
+alias mail_count_trash='mailbox_count Trash'
 
