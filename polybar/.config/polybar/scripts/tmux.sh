7de9feab365d1f59f7291b2f55634d4074b3e344
tong <tong@disktree.net>
2023-03-17 10:21:06 +0100

    Update polybar config

--- polybar/.config/polybar/scripts/tmux.sh
+++ polybar/.config/polybar/scripts/tmux.sh
@@ -7,7 +7,8 @@ if sessions=$(tmux ls 2>/dev/null); then
     echo "$sessions" | while read -r line; do
         session=$(echo "$line" | cut -d ':' -f 1)
         if echo "$line" | grep -q "(attached)"; then
-            color="%{B$bg}%{F$fg}"
+           # color="%{B$bg}%{F$fg}"
+            color="%{B#00}%{F$fg}"
         else
             color="%{B#00}%{F$fg_low}"
         fi
