05fb31b09a876048e20689ff30ccee8683f78065
tong <tong@disktree.net>
2023-05-11 18:58:25 +0200

    Print notification list in waybars' github module tooltip

--- waybar/.config/waybar/modules/github.sh
+++ waybar/.config/waybar/modules/github.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 notifications=$(gh api \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   /notifications)
 count=$(echo "$notifications" | jq '. | length')
-if [[ "$count" != "0" ]]; then
-    #tooltip=""
-    #class=""
-    echo '{"text":'$count',"tooltip":"$tooltip","class":"$class"}'
+#subjects=$(echo "$notifications" | jq -r '.[].subject.title' |  tr -d '\n')
+if [ "$count" != "0" ]; then
+    TOOLTIP=$(echo "$notifications" |jq -r '.[] | { repo:.repository.full_name, subject:.subject.title, id:.id } | "<b>"+.repo+"</b> <tt>"+.subject+"</tt>"' | while read -r line; do printf "<span>%s</span>\r" "$line"; done)
+    printf '{"text": "%s", "alt": "%s", "tooltip": "%s", "class": "%s", "percentage": %i }' "$count" "" "$TOOLTIP" "" ""
 fi
