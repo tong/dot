725596363984f2d98e34a861da32a9fcf5c059ef
tong <tong@disktree.net>
2023-05-20 21:23:29 +0200

    [zsh] Add fzf-pass function

--- zsh/.config/zsh/functions/fzf.zsh
+++ zsh/.config/zsh/functions/fzf.zsh
@@ -1,4 +1,4 @@
-fgitlog() {
+fzf-git-log() {
     log=$(git log --graph --color=always --format="%C(auto)%h%d %s %C(red)%C(bold)%an %C(black)%cr - %cD" "$@")
     if (( $? )) ; then
         return 1
@@ -6,16 +6,11 @@ fgitlog() {
     echo $log | fzf --prompt=" " --height 80% --ansi --no-sort --reverse --tiebreak=index --bind=ctrl-s:toggle-sort --bind "ctrl-m:execute:(grep -o '[a-f0-9]\{7\}' | head -1 | xargs -I % sh -c 'git show --color=always % | less -R') << 'FZF-EOF {} FZF-EOF"
 }
 
-# TODO can improve that with a bind to switch to what was installed
-# fpac() {
-#     pacman -Slq | fzf --multi --reverse --preview 'pacman -Si {1}' | xargs -ro sudo pacman -S
-# }
-#
-# fyay() {
-#     yay -Slq | fzf --multi --reverse --preview 'yay -Si {1}' | xargs -ro yay -S
-# }
+fzf-man() {
+    f=$(fd . $MANPATH/man${1:-1} -t f -x echo {/.} | fzf) && man $f
+}
 
-ftmux() {
+fzf-tmux() {
     if [[ ! -n $TMUX ]]; then
         ID="`tmux list-sessions`"
         if [[ -z "$ID" ]]; then
@@ -35,37 +30,22 @@ ftmux() {
     fi
 }
 
-# --- Other ---
-
-# List install files for dotfiles
-# fdot() {
-#     file=$(find "$DOTFILES/install" -exec basename {} ';' | sort | uniq | nl | fzf | cut -f 2)
-#     [ -n "$file" ] && "$EDITOR" "$DOTFILES/install/$file"
-# }
-
-fman() {
-    f=$(fd . $MANPATH/man${1:-1} -t f -x echo {/.} | fzf) && man $f
+fzf-pass() {
+    HEADER="Selected password will be copied to clipboard"
+    [ "$1" = "show" ] && HEADER="Selected password will be printed to stdout"
+    id=$(find ~/.password-store -type f | while read PW; do echo "${PW%.*}" | cut -d'/' -f5-6; done | fzf --prompt="󱅞 " --pointer="" --border=double --header="$HEADER" --color='dark,fg+:red')
+    pw=$(pass "$id")
+    [ -z "$pw" ] && return 1
+    if [ "$1" = "show" ]; then
+        echo "$pw"
+    else
+        echo "$pw" | kitty +kitten clipboard
+        echo "Copied to clipboard"
+    fi
 }
 
-fpdf() {
+fzf-pdf() {
     result=$(find -type f -name '*.pdf' | fzf --bind "ctrl-r:reload(find -type f -name '*.pdf')" --preview "pdftotext {} - | less")
     [ -n "$result" ] && zathura "$result" &
 }
 
-# fepub() {
-#     result=$(find -type f -name '*.epub' | fzf --bind "ctrl-r:reload(find -type f -name '*.epub')")
-#     [ -n "$result" ] && zathura "$result" &
-# }
-
-# fpop() {
-#     # Only work with alias d defined as:
-#     # alias d='dirs -v'
-#     # for index ({1..9}) alias "$index"="cd +${index}"; unset index
-#     d | fzf --height="20%" | cut -f 1 | source /dev/stdin
-# }
-
-# List projects
-# fwork() {
-#     result=$(find ~/workspace/* -type d -prune -exec basename {} ';' | sort | uniq | nl | fzf | cut -f 2)
-#     [ -n "$result" ] && cd ~/workspace/$result
-# }
