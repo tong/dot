005bc5d75b7f304f7b2d77d80bcf13c44375d3a6
tong <tong@disktree.net>
2023-11-06 13:44:50 +0100

    [zsh] Fix mailcount aliases

--- zsh/.config/zsh/functions/mail.zsh
+++ zsh/.config/zsh/functions/mail.zsh
@@ -1,10 +1,10 @@
 mailbox_count() {
-    find "$HOME/.local/share/mail/$EMAIL/$1/cur" -type f | wc -l
+    find "$HOME/.local/share/mail/$EMAIL/$1" -type f | wc -l
 }
-alias mail_count_archive='mailbox_count Archive'
-alias mail_count_drafts='mailbox_count Drafts'
-alias mail_count_inbox='mailbox_count INBOX'
-alias mail_count_junk='mailbox_count Junk'
-alias mail_count_sent='mailbox_count Sent'
-alias mail_count_trash='mailbox_count Trash'
+alias mail_count_archive='mailbox_count Archive/cur'
+alias mail_count_drafts='mailbox_count Drafts/cur'
+alias mail_count_inbox='mailbox_count INBOX/cur'
+alias mail_count_junk='mailbox_count spambucket/new'
+alias mail_count_sent='mailbox_count Sent/cur'
+alias mail_count_trash='mailbox_count Trash/cur'
 
