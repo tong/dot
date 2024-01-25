c5c1c7066a0c01b5d5f6196dab6adcf1aef350f4
tong <tong@disktree.net>
2024-01-25 12:40:04 +0100

    [zsh] Update fzf functions

--- zsh/.config/zsh/functions/fzf.zsh
+++ zsh/.config/zsh/functions/fzf.zsh
@@ -10,7 +10,7 @@ fzf-chrome-history() {
     sqlite3 -separator $sep /tmp/h \
         "select substr(title, 1, $cols), url from urls order by last_visit_time desc" |
         awk -F $sep '{printf "%-'$cols's  \x1b[36m%s\x1b[m\n", $1, $2}' |
-        fzf --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs xdg-open > /dev/null 2> /dev/null
+        fzf --prompt=" " --ansi --multi | sed 's#.*\(https*://\)#\1#' | xargs xdg-open > /dev/null 2> /dev/null
 }
 
 fzf-git-log() {
