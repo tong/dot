075af816a43f49d1bd62fb660669a0a63b757ca3
tong <tong@disktree.net>
2025-09-21 20:20:47 +0200

    [zsh] cleanup keybinding config

--- zsh/.config/zsh/keybindings.zsh
+++ zsh/.config/zsh/keybindings.zsh
@@ -5,38 +5,43 @@ zle -N down-line-or-beginning-search
 
 # Start typing + [Up-Arrow] - fuzzy find history forward
 if [[ -n "${terminfo[kcuu1]}" ]]; then
-    autoload -U up-line-or-beginning-search
-    zle -N up-line-or-beginning-search
+    # autoload -U up-line-or-beginning-search
+    # zle -N up-line-or-beginning-search
+    bindkey "${terminfo[kcuu1]}" up-line-or-beginning-search 
     bindkey -M viins "${terminfo[kcuu1]}" up-line-or-beginning-search
     bindkey -M vicmd "${terminfo[kcuu1]}" up-line-or-beginning-search
 fi
 # Start typing + [Down-Arrow] - fuzzy find history backward
 if [[ -n "${terminfo[kcud1]}" ]]; then
-    autoload -U down-line-or-beginning-search
-    zle -N down-line-or-beginning-search
+    # autoload -U down-line-or-beginning-search
+    # zle -N down-line-or-beginning-search
+    bindkey "${terminfo[kcud1]}" down-line-or-beginning-search 
     bindkey -M viins "${terminfo[kcud1]}" down-line-or-beginning-search
     bindkey -M vicmd "${terminfo[kcud1]}" down-line-or-beginning-search
 fi
 
 # [Home] - Go to beginning of line
 if [[ -n "${terminfo[khome]}" ]]; then
+    bindkey "${terminfo[khome]}" beginning-of-line  
     bindkey -M viins "${terminfo[khome]}" beginning-of-line
     bindkey -M vicmd "${terminfo[khome]}" beginning-of-line
 fi
 # [End] - Go to end of line
 if [[ -n "${terminfo[kend]}" ]]; then
+    bindkey "${terminfo[kend]}" end-of-line   
     bindkey -M viins "${terminfo[kend]}"  end-of-line
     bindkey -M vicmd "${terminfo[kend]}"  end-of-line
 fi
 
 # [Shift-Tab] - move through the completion menu backwards
 if [[ -n "${terminfo[kcbt]}" ]]; then
+    bindkey "${terminfo[kcbt]}" reverse-menu-complete  
     bindkey -M viins "${terminfo[kcbt]}" reverse-menu-complete
     bindkey -M vicmd "${terminfo[kcbt]}" reverse-menu-complete
 fi
 
-bindkey "^[[A" up-line-or-beginning-search # Up
-bindkey "^[[B" down-line-or-beginning-search # Down
+bindkey "^[[A" up-line-or-beginning-search
+bindkey "^[[B" down-line-or-beginning-search
 
 bindkey -s '^o' 'lfcd\n'
 
@@ -45,7 +50,13 @@ autoload -U edit-command-line
 zle -N edit-command-line
 bindkey '\C-x\C-e' edit-command-line
 
-# By default, Ctrl+d will not close your shell if the command line is filled, this fixes it
-exit_zsh() { exit }
+# Exit zsh if command line is empty, otherwise clear it
+exit_zsh() {
+  if [[ -z "$LBUFFER" ]]; then
+    exit
+  else
+   zle .send-break
+  fi
+} 
 zle -N exit_zsh
 bindkey '^D' exit_zsh
